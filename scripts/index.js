cd306f88a88cb8dea280745ab7a9f86cd61f9c45
Liubov Sereda <lbvsereda@gmail.com>
2021-10-10 17:38:17 +0300

    feat: add edit profile form

---
+++ scripts/index.js
@@ -0,0 +1,58 @@
+const profile = document.querySelector('.profile');
+const profilePopup = document.querySelector('.popup');
+
+const profileEditButton = profile.querySelector('.profile__edit-button');
+const profileCloseButton = profilePopup.querySelector('.popup__close-button');
+
+const profileForm = profilePopup.querySelector('.popup__form');
+const nameField = profilePopup.querySelector('.popup__input_type_name');
+const descirptionField = profilePopup.querySelector('.popup__input_type_description');
+
+let profileInfo = {
+  "name": profile.querySelector('.profile__name'),
+  "description": profile.querySelector('.profile__description')
+};
+
+
+function openProfile(){
+  nameField.value = profileInfo.name.textContent;
+  descirptionField.value = profileInfo.description.textContent;
+
+  profilePopup.classList.add('popup_opened');
+}
+
+
+function closeProfile() {
+  profilePopup.classList.remove('popup_opened');
+}
+
+
+function closePopupHandler(event) {
+  if (event.target.classList.contains('popup')) {
+    closeProfile();
+  }
+}
+
+
+function submitProfileForm(event) {
+  event.preventDefault();
+
+  profileInfo.name.textContent = nameField.value;
+  profileInfo.description.textContent = descirptionField.value;
+
+  closeProfile();
+}
+
+
+profileEditButton.addEventListener('click', openProfile);
+
+profileCloseButton.addEventListener('click', closeProfile);
+profilePopup.addEventListener('mouseup', closePopupHandler);
+
+profileForm.addEventListener('submit', submitProfileForm);
+
+
+
+
+
+
